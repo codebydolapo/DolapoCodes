857c249c833644e84622e4585f8141c6031cf1f1
codebydolapo <bashorun115@gmail.com>
2026-03-12 09:38:09 +0100

    update resume link

--- data/resume.tsx
+++ data/resume.tsx
@@ -1,3 +1,3 @@
-const resume = "https://drive.google.com/file/d/1Upw1JYp2y_v-AkLEy0GBV5lwjYnVT9fD/view?usp=drive_link"
+const resume = "https://drive.google.com/file/d/1bIJPfQJZHOr6fjaRXeD3m2jD27_na4L0/view?usp=sharing"
 
 export default resume
